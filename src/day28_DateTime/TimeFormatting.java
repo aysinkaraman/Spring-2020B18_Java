1e3336a1d05117077bd907ccf10cee56dc20f540
aysinkaraman <aysinkaraman5@gmail.com>
2020-04-24 11:07:02 -0400

    new folders

--- src/day28_DateTime/TimeFormatting.java
+++ src/day28_DateTime/TimeFormatting.java
@@ -1,4 +1,16 @@
 package day28_DateTime;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class TimeFormatting {
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a");
+        LocalTime time1 = LocalTime.of(16,30);
+        System.out.println(time1.format(dtf));
+        LocalTime time2 = LocalTime.of(10,0);
+        System.out.println(time2.format(dtf));
+        boolean result1 = time1.isAfter(time2);
+        System.out.println(result1);
+    }
 }
